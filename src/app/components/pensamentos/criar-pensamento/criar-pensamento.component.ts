6d822f2fdbd5820909b3cbd7505496305288d654
gabisegger <gabi.segger@gmail.com>
2024-02-28 09:47:24 -0300

    property, event e data binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class CriarPensamentoComponent {
 
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+
+  criarPensamento(){
+    alert('Novo pensamento criado!')
+  }
+
+  cancelar(){
+    alert('Ação cancelada!')
+  }
 }
